2c8967d4146e88e0cbafce73d326093f95c8c701
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-15 19:25:59 -0300

    Home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Header from '../Header/Header.jsx';
+
 function Home() {
-    return (<h1>Home</h1>);
+    return (
+        <>
+            <Header name="Home" />
+
+            <div className="jumbotron">
+                <h1 className="display-4"><strong>Olá,</strong></h1>
+
+                <p className="lead">Este sistema foi desenvolvido para facilitar parte da administração de sua</p>
+                <p className="lead">escola e estamos felizes em ter você aqui!</p>
+
+                <a className="btn btn-primary btn-lg" href="#" role="button">Saiba mais</a>
+            </div>
+        </>
+    );
 }
 
 export default Home;
