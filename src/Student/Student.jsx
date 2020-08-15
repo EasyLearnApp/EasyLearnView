5d0018070669a5711a485680be436caf6b4126ed
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-15 19:27:15 -0300

    Student

--- src/Student/Student.jsx
+++ src/Student/Student.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Header from '../Header/Header.jsx';
+import Folder from '../Folder/Folder.jsx';
+
 function Student() {
-    return (<h1>Student</h1>);
+    return (
+        <>
+            <Header />
+
+            <Folder name="Alunos" />
+        </>
+    );
 }
 
 export default Student;
