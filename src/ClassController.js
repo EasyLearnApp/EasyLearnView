f008fb11614fcb7c7e61b2e702a28adb79dba89d
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-16 20:16:48 -0300

    Added Methods to ClassController

--- src/ClassController.js
+++ src/ClassController.js
@@ -4,6 +4,9 @@ class ClassController {
     /** @var {String} */
     static ALL = "http://swvschools.swvsoftware.com.br/api/turmas";
 
+    /** @var {String} */
+    static POST = "http://swvschools.swvsoftware.com.br/api/setturma";
+
     /**
      * Método responsável pela obtenção de uma listagem geral.
      * 
@@ -13,6 +16,30 @@ class ClassController {
      */
     index() {
         return Axios.get(ClassController.ALL);
+    }
+
+    /**
+     * Método responsável pelo cadastro de um aluno.
+     * 
+     * @param {Object} params 
+     */
+    create(params) {
+        return Axios.post(ClassController.POST, params);
+    }
+
+    /**
+     * Realiza uma busca a partir de determinados critérios.
+     * 
+     * @param {Array} params
+     */
+    display(params) {
+        if (params.length === 0) {
+            return (new ClassController()).index();
+        }
+
+        const Url = `${ClassController.ALL}?${params.join('&')}`;
+
+        return Axios.get(Url);
     }
 }
 
