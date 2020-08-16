f6c3a170980e0b1a460db9f1d1b3f2d18f986ad7
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-16 18:59:06 -0300

    FormInput

--- src/FormInput/FormInput.jsx
+++ src/FormInput/FormInput.jsx
@@ -3,15 +3,25 @@ import React from 'react';
 function FormInput(props) {
     return (
         <div className="input-group input-group-lg pb-1">
+
             <div className="input-group-prepend">
+
                 <span className="input-group-text">
+
                     <props.icon />
+
                 </span>
+
             </div>
-            <input type="text" className="form-control fix-rounded-right" placeholder={props.placeholder} required />
+
+            <input value={props.value} onChange={e => { (props.setter)(e.target.value) }} type={props.type} className="form-control fix-rounded-right" placeholder={props.placeholder} required />
+
             <div className="invalid-feedback">
+
                 {props.error}
+
             </div>
+
         </div>
     );
 }
