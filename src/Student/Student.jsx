6e279632bf8c40caa1eea8783bf95c691affcd90
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-08-16 18:59:38 -0300

    Student

--- src/Student/Student.jsx
+++ src/Student/Student.jsx
@@ -13,6 +13,7 @@ import { FaUserAlt, FaRegCalendarAlt, FaRegCircle } from "react-icons/fa";
 import { TiGroup } from 'react-icons/ti';
 
 import Alert from '../Alert/Alert.jsx';
+import Listing from '../Listing/Listing.jsx';
 
 /** @var {Array} */
 const PATH = ['Home', 'Library', 'Data'];
@@ -34,16 +35,23 @@ function Student() {
     const [modalShow, setModalShow] = useState({ msg: "", title: "", display: false });
 
     /** @var {Array} */
-    const [inputName, setInputName] = useState('');
+    const [modalListing, setModalListing] = useState({ data: [], header: [], title: "", display: false });
 
+    /** @var {Array} */
+    const [inputName, setInputName] = useState('');
     /** @var {Array} */
     const [inputDateOfBirth, setInputDateOfBirth] = useState('');
-
     /** @var {Array} */
     const [inputSex, setInputSex] = useState('');
-
     /** @var {Array} */
     const [inputClass, setInputClass] = useState('');
+
+    /** @var {Array} */
+    const [searchName, setSearchName] = useState('');
+    /** @var {Array} */
+    const [searchSex, setSearchSex] = useState('');
+    /** @var {Array} */
+    const [searchClass, setSearchClass] = useState('');
 
     /** @var {Array} */
     const [classes, setClasses] = useState([]);
@@ -109,55 +117,117 @@ function Student() {
         }
     }
 
+    /**
+     * Função responsável pela busca do aluno a partir dos filtros.
+     * 
+     * @param {Void}
+     */
+    function handleStudentSearch() {
+
+    }
+
+    /**
+     * Retorna uma listagem de todos os estudantes.
+     * 
+     * @param {Void}
+     */
+    function handleGetAllStudents() {
+        const response = (new StudentController()).index();
+
+        response.then(success => {
+            const data = (success.data).map(student => {
+                return [
+                    student.nome,
+                    student.data_nascimento,
+                    student.sexo == "M" ? "Masculino" : "Feminino",
+                    student.nome_turma,
+                ];
+            });
+
+            setModalListing(
+                {
+                    header: ["Aluno", "Data de Nascimento", "Sexo", "Turma"],
+                    data: data,
+                    title: "Resultado da busca - Alunos",
+                    display: true
+                });
+        }).catch(err => {
+            setModalShow({ msg: err.response.data.erro, title: "Resultado da busca - Alunos", display: true });
+        });
+    }
+
     return (
         <>
             <Header />
 
             <Folder name="Alunos" path={PATH} />
 
-            <div className="row pt-3">
+            <div className="container-fluid content-row">
+                <div className="row pt-3">
 
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">
-                                <strong>Cadastrar Aluno</strong>
-                            </h5>
+                    <div className="col-md-6">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <strong>Cadastrar Aluno</strong>
+                                </h5>
+                                <div className="row">
+                                    <FormInput icon={FaUserAlt} error="Informe o nome" type="text" value={inputName} setter={setInputName} placeholder="Nome Completo do Aluno" />
 
-                            <FormInput icon={FaUserAlt} error="Informe o nome" type="text" value={inputName} setter={setInputName} placeholder="Nome Completo do Aluno" />
+                                    <FormInput icon={FaRegCalendarAlt} error="Informe a Data de Nascimento" type="date" value={inputDateOfBirth} setter={setInputDateOfBirth} placeholder="Data de Nascimento do Aluno" />
 
-                            <FormInput icon={FaRegCalendarAlt} error="Informe a Data de Nascimento" type="date" value={inputDateOfBirth} setter={setInputDateOfBirth} placeholder="Data de Nascimento do Aluno" />
+                                    <Select icon={FaRegCircle} data={SEXO} error="Informe o Sexo" value={inputSex} setter={setInputSex} hidden="Sexo" />
 
-                            <Select icon={FaRegCircle} data={SEXO} error="Informe o Sexo" value={inputSex} setter={setInputSex} hidden="Sexo" />
-
-                            <Select icon={TiGroup} data={classes} error="Informe a Turma" value={inputClass} setter={setInputClass} hidden="Turma" />
-
-                            <div className="d-flex flex-row-reverse">
-                                <div className="p-3">
-                                    <button type="button" onClick={handleSubmitRecordStudent} className="btn btn-primary btn-lg">Cadastrar Aluno</button>
+                                    <Select icon={TiGroup} data={classes} error="Informe a Turma" value={inputClass} setter={setInputClass} hidden="Turma" />
                                 </div>
+                                <div className="row justify-content-end">
+                                    <div className="p-3">
+                                        <button type="button" onClick={handleSubmitRecordStudent} className="btn btn-primary btn-lg">Cadastrar Aluno</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-6">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <strong>Pesquisar Aluno</strong>
+                                </h5>
+                                <div className="row">
+                                    <FormInput icon={FaUserAlt} error="Informe o nome" type="text" value={searchName} setter={setSearchName} placeholder="Nome Completo do Aluno" />
+
+                                    <Select icon={FaRegCircle} data={SEXO} error="Informe o Sexo" value={searchSex} setter={setSearchSex} hidden="Sexo" />
+
+                                    <Select icon={TiGroup} data={classes} error="Informe a Turma" value={searchClass} setter={setSearchClass} hidden="Turma" />
+                                </div>
+                                <div className="row justify-content-end">
+                                    <div className="p-3">
+                                        <button type="button" onClick={handleStudentSearch} className="btn btn-primary btn-lg">Buscar Aluno</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Alert data={modalShow} show={modalShow.display} onHide={() => setModalShow({ display: false })} />
+                </div>
+
+                <div className="card mb-3 mt-5">
+                    <div className="row no-gutters">
+                        <div className="col-md-10">
+                            <div><strong>Visualizar Alunos</strong></div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="p-3">
+                                <button type="button" onClick={handleGetAllStudents} className="btn btn-primary btn-lg">Exibir</button>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="col-md-6">
-                    <div className="card">
-                        <div className="card-body">
-
-                            <h5 className="card-title">
-                                <strong>Pesquisar Aluno</strong>
-                            </h5>
-
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-
-                            <button type="button" className="btn btn-primary">Go somewhere</button>
-                        </div>
-                    </div>
-                </div>
-
-                <Alert data={modalShow} show={modalShow.display} onHide={() => setModalShow({ display: false })} />
-
+                <Listing info={modalListing} show={modalListing.display} onHide={() => setModalListing({ data: [], header: [], title: "", display: false })} />
             </div>
         </>
     );
