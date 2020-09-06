33ca2a8d673d73b0df8e59c174a4b870bf3916b0
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-06 20:32:49 -0300

    Updated Copyright

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Copyright() {
-    const COPYRIGHT_MESSAGE_DEFAULT = `@SWV Software ${(new Date()).getFullYear()} - Todos os direitos reservados`;
+    const COPYRIGHT_MESSAGE_DEFAULT = `@EasyLearn ${(new Date()).getFullYear()} - Todos os direitos reservados`;
 
     return (<><hr/><p>{COPYRIGHT_MESSAGE_DEFAULT}</p></>);
 }
