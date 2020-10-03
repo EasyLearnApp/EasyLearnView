9b2c573e36b427bc3754275ea653e1cd93915d32
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-03 02:31:55 -0300

    Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Card(props) {
+    const __MESSAGE = `Gerencie os ${(props.label).toLowerCase()} de uma forma fácil, cadastre, edite e pesquise com poucos cliques.`;
+
     return (
         <div className="col-sm-6">
             <div className="card card-width">
                 <div className="card-body">
                     <h5 className="card-title">{props.label}</h5>
 
-                    <p className="card-text">Gerencie os {(props.label).toLowerCase()} de uma forma fácil, cadastre, edite e pesquise com poucos cliques.</p>
+                    <p className="card-text">{__MESSAGE}</p>
 
                     <Link to={props.url} className="btn btn-primary">Gerenciar {props.label}</Link>
                 </div>
