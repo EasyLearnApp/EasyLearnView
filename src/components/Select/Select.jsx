846efa29b5947e62c93ba0223d7562739210b14d
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-10-03 02:32:54 -0300

    Select.jsx

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -3,31 +3,22 @@ import React from 'react';
 function Select(props) {
     return (
         <div className="input-group input-group-lg pb-1">
-
             <div className="input-group-prepend">
-
                 <span className="input-group-text">
-
                     <props.icon />
-
                 </span>
-
             </div>
 
             <select value={props.value} onChange={e => { (props.setter)(e.target.value) }} className="custom-select" required>
-
                 <option hidden>{props.hidden}</option>
-
                 {
                     (props.data).map((value, key) => {
                         return (<option key={key} value={value.value}>{value.label}</option>)
                     })
                 }
-
             </select>
 
             <div className="invalid-feedback">{props.error}</div>
-
         </div>
     );
 }
